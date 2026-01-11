62583d23281ac8b60d934aae51f9cd477d4ce493
Marko <markoilioski21@gmail.com>
2026-01-11 12:10:11 +0100

    Better char and movment and started controller support.

--- player.gd
+++ player.gd
@@ -5,12 +5,19 @@ extends CharacterBody3D
 @onready var _camera_yaw: Node3D = self
 @onready var _camera_pitch: Node3D = $Camera3D
 
+const SPEED_WALK = 3      # Horizontal movement speed (pixels/second)
+const SPEED_SPRINT = SPEED_WALK * 2       # Horizontal movement speed (pixels/second)
+const JUMP_VELOCITY = -300.0 # Jump strength (negative because Y goes down)
+
 const CAMERA_MAX_PITCH: float = deg_to_rad(70)
 const CAMERA_MIN_PITCH: float = deg_to_rad(-89.9)
 const CAMERA_RATIO: float = .625
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+# Variables for "game feel" techniques (we'll initialize them later)
+var sprint = false
+var coyote_timer = 0.0
+const COYOTE_TIME_THRESHOLD = 0.1 # 100 milliseconds of coyote time
+
 var mouse_sensitivity = 0.002
 
 func _ready() -> void:
@@ -22,21 +29,28 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var max_speed = SPEED_WALK
+	# Handle sprint.
+	if Input.is_action_just_pressed("sprint"):
+		sprint = true
+		max_speed = SPEED_SPRINT
+	elif Input.is_action_just_released("sprint"):
+		sprint = false
+		max_speed = SPEED_WALK
+	elif sprint:
+		max_speed = SPEED_SPRINT
+	
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_len_sqrt := input_dir.length_squared()
+	var input_len_delta := input_len_sqrt/10
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = move_toward(velocity.x, direction.x * input_len_sqrt * max_speed , input_len_delta)
+		velocity.z = move_toward(velocity.z, direction.z * input_len_sqrt * max_speed , input_len_delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-	print(input_dir)
+		velocity.x = move_toward(velocity.x, 0, max_speed/3)
+		velocity.z = move_toward(velocity.z, 0, max_speed/3)
+	print("Max Velocity: ", direction.x * input_len_sqrt * max_speed, " Current Velocity: ", velocity, " Acceleration: ", input_len_delta)
 	move_and_slide()
 
 func _input(p_event: InputEvent) -> void:
